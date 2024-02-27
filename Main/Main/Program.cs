4973c0d346b3e910c2c71f4909c81984ad766483
DrKali7 <agnus753@outlook.com>
2024-02-28 02:00:22 +0400

    ASD
    ASD

---
+++ Main/Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x-5);
+        }
+    }
+}
